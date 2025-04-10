5da2ffe6f76f9e23e3045c42df463a63419339a9
Teeradech Thanapop <t.thanapop@gmail.com>
2025-04-10 10:47:18 +0700

    feat: init project

---
+++ release.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  dryRun: false,
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/changelog',
+      {
+        changelogFile: 'docs/CHANGELOG.md',
+      },
+    ],
+    '@semantic-release/npm',
+    [
+      '@semantic-release/github',
+      {
+        releasedLabels: ['released', 'v<%= nextRelease.version %>'],
+      },
+    ],
+    [
+      '@semantic-release/git',
+      {
+        assets: ['package.json', 'docs'],
+        message:
+          'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+      },
+    ],
+  ],
+};
