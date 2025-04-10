758a3849679639ba1da86017242de0fdd4ce0eca
Teeradech Thanapop <teeradech-th@users.noreply.github.com>
2025-04-10 19:35:33 +0700

    fix(release): update prerelease config (#32)

--- release.config.js
+++ release.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   dryRun: false,
+  branches: [
+    { name: 'main' },
+    { name: 'next', prerelease: 'next' },
+  ],
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
